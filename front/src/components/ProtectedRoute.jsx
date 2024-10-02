c700cdc3d33b78fe70bb43862211308abdfe8990
Kaleab Endrias <kaleabendrias1212@gmail.com>
2024-10-02 12:50:39 +0300

    feat: repaired all the complains

--- front/src/components/ProtectedRoute.jsx
+++ front/src/components/ProtectedRoute.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 
@@ -9,6 +10,10 @@ const ProtectedRoute = ({ children }) => {
   }
 
   return children;
+};
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired
 };
 
 export default ProtectedRoute;
